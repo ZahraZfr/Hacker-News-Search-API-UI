bb79339c33916409df86f4aeca08fcdd184e6680
ZahraZfr <Zahra.Z@m.lo.agency>
2021-10-09 17:15:28 +0330

    feat: initial settup

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+import App from "./App";
+
+console.log(process.env.REACT_APP_DNS_URL);
+
+Sentry.init({
+  dsn: process.env.REACT_APP_DNS_URL,
+  integrations: [new Integrations.BrowserTracing()],
+
+  tracesSampleRate: 1.0,
+});
+
+
+  
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
