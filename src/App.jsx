dd1e562a27680e7b7752b03049862a3e2adecbb1
ZahraZfr <Zahra.Z@m.lo.agency>
2021-10-11 19:44:59 +0330

    feat: add style to header and add search section

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-import './App.css';
-import axios from 'axios';
-import { useEffect , useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import React from "react";
-import Header from './component/Header';
+import Header from "./component/Header";
+import Main from "./component/Main";
 
 function App() {
-  const [users , setUsers] = useState([]);
+  const [users, setUsers] = useState([]);
   useEffect(() => {
-    axios.get("http://hn.algolia.com/api/v1/search?query=")
-    .then(res => {
-      setUsers(res.data.hits);
-      console.log(res.data.hits);
-    })
-    .catch();
-  },[]);
-
+    axios
+      .get("http://hn.algolia.com/api/v1/search_by_date?tags=story")
+      .then((res) => {
+        setUsers(res.data.hits);
+        // console.log(res.data.hits);
+      })
+      .catch();
+  }, []);
 
   return (
     <div className="App">
       <Header />
-      <ul className="bg-red-200">
-        {users.map(news =>{
-          return <div><a href= {news.url}><p key={news.id}> {news.title}</p></a><p>{news.author}</p> </div>
-                  
-        })}
-      </ul>
+      <Main users = {users}/>
     </div>
   );
 }
 
 export default App;
+
+
+
