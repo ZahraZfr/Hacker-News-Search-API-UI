bd2ffab30dfbf2e6c54db009316e0f6e65e8ad8d
ZahraZfr <Zahra.Z@m.lo.agency>
2021-10-12 22:29:29 +0330

    fix: clean code in filter and map section

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,25 @@ function App() {
       })
       .catch();
   }, [source]);
-
+  function showData(item){
+    return item.filter(item => item.title).map((news, index) => {
+      return (
+        <div key={index} className="p-15 p-3 text-gray-900">
+          <a className="mt-5" href={news.url}>
+            <p className="font-medium"> {news.title}</p>
+          </a>
+          <p className="font-normal text-xs">
+            <span>points : {news.points}</span>
+            <span> by {news.author} </span>
+            <span>
+              | at: {news.created_at.slice(0, 10)} | Time:{" "}
+              {news.created_at.slice(12, 19)}{" "}
+            </span>
+          </p>
+        </div>
+      );
+    })
+  }
   return (
     <div className="App">
       {/* header */}
@@ -45,23 +63,8 @@ function App() {
       <div className="bg-gray-200 mt-40 w-full">
         
 
-        {item.map((news, index) => {
-          return (
-            <div key={index} className="p-15 p-3 text-gray-900">
-              <a className="mt-5" href={news.url}>
-                <p className="font-medium"> {news.title}</p>
-              </a>
-              <p className="font-normal text-xs">
-                <span>points : {news.points}</span>
-                <span> by {news.author} </span>
-                <span>
-                  | at: {news.created_at.slice(0, 10)} | Time:{" "}
-                  {news.created_at.slice(12, 19)}{" "}
-                </span>
-              </p>
-            </div>
-          );
-        })}
+        
+        {showData(item)}
       </div>
       {/* footer*/}
       <Footer />
