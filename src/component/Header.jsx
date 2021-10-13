d7111f178b90aebcf635192d3d97aaa756ff61dc
ZahraZfr <Zahra.Z@m.lo.agency>
2021-10-14 03:23:22 +0330

    feat: add set timeout

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import {MyContext} from "./MyContext"
+import { MyContext } from "./MyContext";
 import { useContext } from "react";
-
+let timer = null;
 const Header = () => {
-  const {setSource} = useContext(MyContext)
+  const { setSource } = useContext(MyContext);
+  
   return (
     <div className="bg-blue-500 font-sans fixed h-36 w-full -mt-36">
       <div className="py-5 font-bold text-3xl text-white flex justify-center ">
@@ -17,14 +18,17 @@ const Header = () => {
           id="input"
           className="mb-5 px-2 mx-1 w-100 rounded-r-2xl text-blue-500 font-bold border-blue-500 outline-none h-12"
           type="search"
-          onChange={(e) => {
-            // get input value to search
-            
-            if (e.target.value === "") {
-              setSource("_by_date?tags=story");
-            } else {
-              setSource(`?query=${e.target.value}`);
-            }
+          onChange={function () {
+            clearTimeout(timer);
+            timer = setTimeout(() => {
+              // get input value to search
+
+              if (document.getElementById("input").value === "") {
+                setSource("_by_date?tags=story");
+              } else {
+                setSource(`?query=${document.getElementById("input").value}`);
+              }
+            }, 2000);
           }}
         />
       </div>
